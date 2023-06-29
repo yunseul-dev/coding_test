be21a93d5860576f3af5ecc93ba21b947b6d999e
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-06-30 00:29:39 +0900

    Update 둘만의 암호

--- Programmers/Level1/둘만의 암호.js
+++ Programmers/Level1/둘만의 암호.js
@@ -1,21 +1,12 @@
-// 테스트케이스 다 안돌아감
 function solution(s, skip, index) {
-  var answer = '';
+  const turn = 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
+    .filter((char) => !skip.includes(char));
 
-  let full = 'abcdefghijklmnopqrstuvwxyz';
-  let turn = [];
-
-  for (let i = 0; i < full.length; i++) {
-    if (![...skip].includes(full[i])) turn.push(full[i]);
-  }
-
-  for (let i = 0; i < s.length; i++) {
-    if (turn.indexOf(s[i]) + index > turn.length - 1) {
-      answer += turn[turn.indexOf(s[i]) + index - turn.length];
-    } else {
-      answer += turn[turn.indexOf(s[i]) + index];
-    }
-  }
-
-  return answer;
+  return [...s]
+    .map((str) => {
+      const newIndex = (turn.indexOf(str) + index) % turn.length;
+      return turn[newIndex];
+    })
+    .join('');
 }
