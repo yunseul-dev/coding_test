22ab5baf1f5f2aa0e51981185556d1b28548cdb8
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-07-10 22:52:55 +0900

    Add

--- Programmers/Level1/이상한 문자 만들기.js
+++ Programmers/Level1/이상한 문자 만들기.js
@@ -1,11 +1,25 @@
 function solution(s) {
-  var answer = '';
-  for (let i = 0; i < s.length; i++) {
-    if (i % 2) {
-      answer += s[i].toLowerCase();
-    } else {
-      answer += s[i].toUpperCase();
+  var sArr = s.split(' ');
+
+  var answer = [];
+  for (let i = 0; i < sArr.length; i++) {
+    var tmp = '';
+    for (let j = 0; j < sArr[i].length; j++) {
+      if (j % 2) {
+        tmp += sArr[i][j].toLowerCase();
+      } else {
+        tmp += sArr[i][j].toUpperCase();
+      }
     }
+    answer.push(tmp);
   }
-  return answer;
+
+  return answer.join(' ');
+}
+
+// 다른 사람 풀이(정규표현식 이용)
+function toWeirdCase(s) {
+  return s.toUpperCase().replace(/(\w)(\w)/g, function (a) {
+    return a[0].toUpperCase() + a[1].toLowerCase();
+  });
 }
