1eff045ce290bf67260d7d459a66457cc5a3deea
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-07-11 14:10:47 +0900

    Create 배열에서 단 한 번만 등장하는 항목 찾기

---
+++ 자료구조와 알고리즘/정렬/2.js
@@ -0,0 +1,34 @@
+// 배열에서 단 한 번만 등장하는 항목 찾기
+
+const findOnlyOnce = (arr, low, high) => {
+  if (low > high) {
+    return null;
+  }
+  if (low === high) {
+    return arr[low];
+  }
+
+  let mid = Math.floor((high + low) / 2);
+
+  if (mid % 2 === 0) {
+    if (arr[mid] === arr[mid + 1]) {
+      return findOnlyOnce(arr, mid + 2, high);
+    } else {
+      return findOnlyOnce(arr, low, mid);
+    }
+  } else {
+    if (arr[mid] === arr[mid - 1]) {
+      return findOnlyOnce(arr, mid + 1, high);
+    } else {
+      return findOnlyOnce(arr, low, mid - 1);
+    }
+  }
+};
+
+const findOnlyOnceHelper = (arr) => {
+  return findOnlyOnce(arr, 0, arr.length);
+};
+
+// 배열의 길이로 어느쪽에 하나의 수가 있는지 찾는다.
+
+console.log(findOnlyOnceHelper([1, 1, 3, 3, 5, 5, 7, 7, 8, 8, 9]));
