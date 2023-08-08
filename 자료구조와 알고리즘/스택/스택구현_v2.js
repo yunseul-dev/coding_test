401bd112513d48e0d3e27d0616fbb25c3ab4f1a1
Cha-Yunseul <117149045+Cha-Yunseul@users.noreply.github.com>
2023-08-08 09:31:41 +0900

    Create 배열을 이용한 스택 구현

---
+++ 자료구조와 알고리즘/스택/스택구현_v2.js
@@ -0,0 +1,21 @@
+// Array 이용
+
+let stack = [];
+
+stack.push(5);
+stack.push(4);
+stack.push(3);
+stack.push(2);
+stack.push(1);
+console.log(stack);
+
+console.log(stack.pop());
+console.log(stack);
+console.log(stack.pop());
+console.log(stack);
+console.log(stack.pop());
+console.log(stack);
+console.log(stack.pop());
+console.log(stack);
+console.log(stack.pop());
+console.log(stack);
